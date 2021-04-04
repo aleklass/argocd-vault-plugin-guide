bfc13ebca3ed4832b3c7f51aa028dbda17e29b15
jamee appiskey <jamee.appiskey@gmail.com>
2021-04-05 01:24:15 +0500

    update script and extra file

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ kubectl exec -ti vault-0 -- vault login
 echo "Enter Vault Token Here"
 kubectl exec -ti vault-0 -- vault auth enable kubernetes 
 
-vault write auth/kubernetes/config \
+kubectl exec -ti vault-0 -- vault write auth/kubernetes/config \
         token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
         kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
         kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
